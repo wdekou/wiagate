623efbd4aa8495d84b2d22d9d5f126801499239f
williamdekou <williamdekou@gmail.com>
2017-11-05 15:37:04 +0100

    Add React app created with creat-react-app

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,10 +1,20 @@
 module.exports = {
-  "extends": "airbnb-base",
+  "parser": "babel-eslint",  
+  "extends": "airbnb",
     "env": {
+      "browser": true,
       "node": true
     },
+    "plugins": [
+      "react",
+    ],
     "rules": {
       "no-restricted-globals": 0,
-      "max-len": 0
+      "max-len": 0,
+      "react/jsx-filename-extension": ["error", { "extensions": [".js", ".jsx"] }],
+      "react/prefer-stateless-function": "off"
+    },
+    "globals": {
+      "document": true
     }
 };
